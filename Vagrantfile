11e3054a435083b464df05c344fe7cc990b6f745
Kelby Ludwig <kelbyludwig@gmail.com>
2015-10-21 14:29:50 -0500

    Working with a partial list of Mallor deps. May put of mallory for now and focus on getting routing working

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,5 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "hashicorp/precise32"
-  #The "provision" line is new, and tells Vagrant to use the shell provisioner to setup the machine, with the bootstrap.sh file. 
-  #The file path is relative to the location of the project root (where the Vagrantfile is).
+  config.vm.box = "hashicorp/precise64"
+  config.vm.network "public_network", ip: "192.168.1.200"
   config.vm.provision :shell, path: "bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y tcpdump
+apt-get upgrade
+#Misc packages that may be nice to have
+apt-get install -y vim curl module-assistant debhelper make
+#Mallory-specific dependencies. Based on this guide https://www.nccgroup.trust/us/about-us/resources/mallory-and-me-setting-up-a-mobile-mallory-gateway/
+apt-get install -y mercurial python-pyasn1 python-netfilter libnetfilter-conntrack-dev python2.7 python2.7-dev python-setuptools
+easy_install pynetfilter_conntrack
+curl http://launchpadlibrarian.net/19436940/netfilter-extensions-source_20080719%2Bdebian-1_all.deb > nfs.deb
+dpkg -i nfs.deb
+apt-get install -y libnetfilter-conntrack3-dbg python-paramiko python-imaging
+#wget http://ubuntu.cs.utah.edu/ubuntu/pool/universe/libn/libnetfilter-conntrack/libnetfilter-conntrack1_0.0.99-1_amd64.deb
+#sudo dpkg -i libnetfilter-conntrack1_0.0.99-1_amd64.deb
+apt-get install hostapd
